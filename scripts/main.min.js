5dbc04782209feeccd55376099e3b0544077ad72
edcarpio <carpio.ed@gmail.com>
2019-11-12 15:56:35 -0800

    updates

--- scripts/main.min.js
+++ scripts/main.min.js
@@ -1,4 +1,5 @@
 $(function() {
+  // Panel Animations
   $("#pageSections .btn").on("click", function() {
     $("body").addClass("panelOpen");
 
@@ -7,5 +8,34 @@ $(function() {
         $("body").addClass("panelOpen");
       } else $("body").removeClass("panelOpen");
     }, 400);
+  });
+
+  // Work Background Images
+  $(".work-column").each(function() {
+    var workBg = $(this).attr("data-workID");
+    var workCaption = $(this).attr("data-workCaption");
+
+    // Apply Background Image
+    $(this)
+      .find(".work-image")
+      .css("background-image", "url(/images/hero-" + workBg + ".png)");
+
+    // Click Action to Expand and Show Detail
+    $(this).on("click", function() {
+      //$("#workCarousel").addClass("active");
+      $(this).addClass("active");
+      $(".work-column:not(.active)").hide();
+
+      $(".work-panel-header")
+        .addClass("active")
+        .html(workCaption);
+    });
+  });
+
+  $(".work-detail-title").on("click", function() {
+    $(".work-column")
+      .removeClass("active")
+      .show();
+    $(".work-panel-header").removeClass("active");
   });
 });
